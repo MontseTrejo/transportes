3933661552dcbdd0461c2a3f7f5e405f3fc3809c
MontseTrejo <95013130+MontseTrejo@users.noreply.github.com>
2021-11-25 16:28:49 -0600

    subo

---
+++ Script/operacion.js
@@ -0,0 +1,80 @@
+// JavaScript Document
+function limpiar(){
+	document.getElementById('formulario').reset();
+}
+
+function carga()
+{
+	var pagar=parseInt(pagar);
+	var peso=0;
+	var vtarifa=0;
+	
+	if(vtarifa=3.1)
+	{
+		pagar=peso*vtarifa;
+	}
+	else if(vtarifa=2.75)
+	{
+		pagar=peso*vtarifa;
+	}
+	else if(vtarifa=3.05)
+	{
+		pagar=peso*vtarifa;
+	}
+	alert("se pagara" + pagar);	
+}
+
+function pasajeros()
+{
+	var pagar=parseInt(pagar);
+	var carga=0;
+	var vtarifa=0;
+	
+	if(vtarifa=22)
+	{
+		pagar=carga*vtarifa;
+	}
+	else if(vtarifa=100)
+	{
+		pagar=carga*vtarifa;
+	}
+	else if(vtarifa=30)
+	{
+		pagar=carga*vtarifa;
+	}
+  alert("se pagara" + pagar);		
+}
+
+function encomiendas()
+{
+	var pagar=parseInt(pagar);
+	var numdoc=0;
+	var vtarifa=0;
+	
+	if(vtarifa=5)
+	{
+		pagar=numdoc*vtarifa;
+	}
+	else if(vtarifa=12)
+	{
+		pagar=numdoc*vtarifa;
+	}
+	else if(vtarifa=7)
+	{
+		pagar=numdoc*vtarifa;
+	}	
+	alert("se pagara" + pagar);			
+}
+
+function animales()
+{
+	var pagar=parseInt(pagar);
+	var peso=0;
+	var vtarifa=0;
+	
+	if(vtarif=25)
+	{
+		pagar=peso*vtarifa
+	}
+	alert("se pagara" + pagar);					
+}
